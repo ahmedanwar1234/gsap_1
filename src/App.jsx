5d5cb06eabe5065c8f2a0193ee7b4fea7f5f849c
ahmedanwar123 <ahmedalprenses@gmail.com>
2024-02-01 00:39:15 +0200

    end from content and image section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+import Content from './Content';
 import Header from './Header';
+import Image from './Image';
 
 function App() {
   return (
     <div className="hero">
 <Header/>
+<div className="conrainer">
+<Content/>
+<Image/>
+
+</div>
     </div>
   );
 }
---
+++ src/Content.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Content.css'
+const Content = () => {
+  return (
+    <div>
+        <div className='content'>
+
+<h1 className="content-inner-bold">
+    <div>Hard work beats talent</div>
+    <br />
+    <div>when talent doesn work hand.</div>
+
+</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque tempore ipsam vitae dolorem enim, impedit iure esse, consequuntur odit nesciunt commodi hic debitis quaerat maiores eius cupiditate quia tenetur officia.  
+</p>
+<button>
+    Explore
+</button>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Content
